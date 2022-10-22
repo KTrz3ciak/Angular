5795facc80c0ae8ba52118c58e70806712848c45
Konrad Trzeciak <trzeciak.konrad96@gmail.com>
2022-10-22 18:08:00 +0200

    New module with new component

---
+++ projectAngular/src/app/new-module/new-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewCompComponent } from './new-comp/new-comp.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewCompComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewModuleModule { }
